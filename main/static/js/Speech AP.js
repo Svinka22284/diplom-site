ca749cff997dc777c81f2ca3f019c371cfec332a
Svinka22284 <Adrianov.Piotr@gmail.com>
2025-12-31 15:16:50 +0200

    Add voice and new css style and new picture

---
+++ main/static/js/Speech AP.js
@@ -0,0 +1,26 @@
+const voiceBtn = document.getElementById("voicebtn");
+const searchInput = document.getElementById("searchinput");
+
+const SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+
+if (!SpeechRecognition) {
+    alert("Ваш браузер не поддерживает голосовой ввод");
+} else {
+    const recognition = new SpeechRecognition();
+    recognition.lang = "uk-UA";
+    recognition.interimResults = false;
+
+    voiceBtn.addEventListener("click", () => {
+        recognition.start();
+    });
+
+    recognition.onresult = (event) => {
+        const text = event.results[0][0].transcript;
+        searchInput.value = text;
+    };
+
+    recognition.onerror = (event) => {
+        console.error("Ошибка:", event.error);
+    };
+}

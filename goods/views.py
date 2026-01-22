5d8a9a74d08d1d4b353e275e82983cae1479aab0
Svinka22284 <Adrianov.Piotr@gmail.com>
2026-01-22 21:16:01 +0200

    Second pagination option - Get params

--- goods/views.py
+++ goods/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404,get_list_or_404
 from goods.models import Products
 # Create your views here.
 def catalog(request,category_slug):
+    page = request.GET.get('page',1)
     if category_slug == 'vse-tovary':
         goods = Products.objects.all()
     else:
-        goods = Products.objects.filter(category__slug=category_slug)
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    paginator = Paginator(goods, 3)
+
+    current_page = paginator.page(int(page))
     context = {
         'title':'Home- Каталог',
-        'goods': goods
+        'goods': current_page,
+        "slug_url": category_slug,
 }
     return render(request, 'goods/catalog.html',context)
 

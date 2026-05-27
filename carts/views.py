520dabe4391ca141be8d875206e69dde9012b228
Svinka22284 <Adrianov.Piotr@gmail.com>
2026-05-27 14:56:57 +0300

    add cart ajax request and json response and cart_items

--- carts/views.py
+++ carts/views.py
@@ -1,22 +1,45 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
 
 from carts.models import Cart
+from carts.utils import get_user_cart
 from goods.models import Products
 
 
 # Create your views here.
-def cart_add(request, product_slug):
-    product = Products.objects.get(slug=product_slug)
+def cart_add(request):
+    product_id = request.POST.get('product_id')
+    product = Products.objects.get(id=product_id)
     if request.user.is_authenticated:
-        carts = Cart.objects.filter(user=request.user, product=product)
+        carts = Cart.objects.filter(
+            user=request.user,
+            product=product
+        )
         if carts.exists():
             cart = carts.first()
-            if cart:
-                cart.quantity += 1
-                cart.save()
+            cart.quantity += 1
+            cart.save()
         else:
-            Cart.objects.create(user=request.user, product=product, quantity=1)
-    return redirect(request.META['HTTP_REFERER'])
+            Cart.objects.create(
+                user=request.user,
+                product=product,
+                quantity=1
+            )
+    user_cart = get_user_cart(request)
+    cart_items_html = render_to_string(
+         "carts/includes/cart_items.html",
+        {"carts": user_cart},
+
+        request=request
+    )
+    total_price = sum(float(cart.product.sell_price()) * cart.quantity for cart in user_cart)
+    response_data = {
+        "message": "Товар додан у кошик",
+        "cart_items_html": cart_items_html,
+        "total_price": total_price,
+    }
+    return JsonResponse(response_data)
 
 
 def cart_change(request, product_slug):

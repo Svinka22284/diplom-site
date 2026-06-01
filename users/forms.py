76164df066dbdf29c432025df4133ffe6b583f4c
Svinka22284 <Adrianov.Piotr@gmail.com>
2026-06-01 18:11:01 +0300

    TEST3

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
-from pkg_resources import require
+
 
 from users.models import User
 
